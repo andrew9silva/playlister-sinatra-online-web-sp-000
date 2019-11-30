c40cbd5dc90516c8e0a605254b5d1e065b356d90
Andrew Silva <andrewsilva9@gmail.com>
2019-11-30 20:52:39 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,12 +24,12 @@ class SongController < ApplicationController
     artist_entry = params[:song][:artist]
     @artist = Artist.find_or_create_by(:name => artist_entry)
 
-    @song.save
-
     genre_selections = params[:song][:genres]
     genre_selections.each do |genre|
       @song.genres << Genre.find(genre)
     end
+
+    @song.save
 
     flash[:message] = "Successfully created song."
     redirect to "songs/#{@song.slug}"
