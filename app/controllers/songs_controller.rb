9332fc1ea339193204d9627be501c6729e21e1d2
Andrew Silva <andrewsilva9@gmail.com>
2019-11-30 19:26:02 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,16 +4,17 @@ class SongController < ApplicationController
     erb :'songs/index'
   end
 
+  get '/songs/new' do
+    @genres = Genre.all
+    erb :'songs/new'
+  end
+
   get '/songs/:slug' do
     slug = params[:slug]
     @song = Song.find_by_slug(slug)
     erb :'songs/show'
   end
 
-  get '/songs/new' do
-    @genres = Genre.all
-    erb :'songs/new'
-  end
 
   post '/songs/new' do
     @song = Song.create(:name => params[:song][:name])
