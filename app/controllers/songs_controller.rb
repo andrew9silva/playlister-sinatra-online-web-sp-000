598b0dd5fe9e176f5a377360fb8e05339fa49a94
Andrew Silva <andrewsilva9@gmail.com>
2019-11-30 19:43:19 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -36,4 +36,30 @@ class SongController < ApplicationController
     @song = Song.find_by_slug(slug)
     erb :'songs/show'
   end
+
+  patch '/songs/:slug' do
+   song = Song.find_by_slug(params[:slug])
+   song.name = params[:song][:name]
+
+   artist_name = params[:song][:artist]
+   if Artist.find_by(:name => artist_name)
+     if song.artist.name != artist_name
+       song.artist = Artist.find_by(:name => artist_name)
+     end
+   else
+     song.artist = Artist.create(:name => artist_name)
+   end
+
+   if song.genres
+     song.genres.clear
+   end
+   genres = params[:song][:genres]
+   genres.each do |genre|
+     song.genres << Genre.find(genre)
+   end
+
+   song.save
+   flash[:message] = "Successfully updated song."
+   redirect to "songs/#{song.slug}"
+ end
 end
